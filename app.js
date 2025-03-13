e6cab0e42f0ca1b46d4ded58c9690f407e146e70
@bboopalan <bboopalan@presidio.com>
2025-03-13 17:46:00 +0530

    Added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const sequelize = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const movieRoutes = require("./routes/movieRoutes");
@@ -10,6 +11,19 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3001'];
+ 
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
+
 
 // Routes
 app.use("/api/auth", authRoutes);
